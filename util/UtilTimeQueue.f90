c42545b2a5e8a9ee779b8076b7ba63116a092dd3
niliu2019 <maotf987654321@gmail.com>
2025-09-22 23:01:33 +0800

    Use Ring buffer logic and bounds check

--- util/UtilTimeQueue.f90
+++ util/UtilTimeQueue.f90
@@ -11,11 +11,14 @@
 Module ModuleTimeQueue
     Implicit None
 
+    ! TimeQueue: A simple queue data structure that maintains data in chronological order
+    ! When the queue is full, adding new data will remove the oldest data (First-In-First-Out)
+    ! This creates a sliding window effect for time-series data
     Type TimeQueue
         Real(8), Allocatable            :: data(:)
         Integer(4)                      :: capacity = 0      ! Maximum size of the queue (fixed capacity)
-        Integer(4)                      :: currentSize = 0  ! Current number of elements in the queue
-        
+        Integer(4)                      :: currentSize = 0   ! Current number of elements in the queue
+        Integer(4)                      :: head = 1          ! Points to the oldest data (queue front)
     contains
         Procedure :: Init
         Procedure :: Enqueue            ! Add a new data point to the queue
@@ -35,24 +38,21 @@ Module ModuleTimeQueue
             Allocate(this%data(this%capacity))
             this%data = 0.0d0
             this%currentSize = 0
+            this%head = 1
         End Subroutine Init
 
         Subroutine Enqueue(this, value)
             Class(TimeQueue), Intent(inout)     :: this
             Real(8), Intent(in)                :: value
-
-            if (this%currentSize == this%capacity) then
-                ! Queue is full, use vectorized operation to shift all elements at once
-                ! This is much faster for large arrays than element-by-element movement
-                ! This implementation only works in Intel API or GNU Fortran
-                this%data(1:this%capacity-1) = this%data(2:this%capacity)
-                
-                ! Add new value at the end
-                this%data(this%capacity) = value
-            else
-                ! Queue is not full, add new element and increase size
+            Integer :: tail
+            if (this%currentSize < this%capacity) then
+                tail = mod(this%head + this%currentSize - 2 + this%capacity*10, this%capacity) + 1
+                this%data(tail) = value
                 this%currentSize = this%currentSize + 1
-                this%data(this%currentSize) = value
+            else
+                tail = this%head
+                this%data(tail) = value
+                this%head = mod(this%head, this%capacity) + 1
             end if
         End Subroutine Enqueue
 
@@ -60,7 +60,7 @@ Module ModuleTimeQueue
             Class(TimeQueue), intent(inout)    :: this
             Integer(4), intent(in)            :: new_capacity
             Real(8), Allocatable              :: new_data(:)
-            Integer :: i, elements_to_copy
+            Integer :: i, elements_to_copy, idx, old_head
 
             if (new_capacity <= 0) then
                 print *, "Error: New size must be greater than 0."
@@ -72,39 +72,16 @@ Module ModuleTimeQueue
             end if
 
             Allocate(new_data(new_capacity))
-            
-            if (new_capacity < this%capacity) then
-                print *, "Warning: New size is smaller than current size. Data may be lost. Keeping the most recent data."
-
-                ! Determine how many elements to copy (the most recent ones)
-                elements_to_copy = min(new_capacity, this%currentSize)
-                
-                ! Copy the most recent elements
-                if (elements_to_copy > 0) then
-                    ! If current queue has more elements than new size, start from newer ones
-                    if (this%currentSize > new_capacity) then
-                        ! Copy newest elements only
-                        do i = 1, elements_to_copy
-                            new_data(i) = this%data(this%currentSize - elements_to_copy + i)
-                        end do
-                    else
-                        ! Copy all existing elements
-                        do i = 1, elements_to_copy
-                            new_data(i) = this%data(i)
-                        end do
-                    end if
-                end if
-                
-                ! Update size
-                this%currentSize = elements_to_copy
-            else
-                ! New capacity is larger, copy all existing data
-                do i = 1, this%currentSize
-                    new_data(i) = this%data(i)
-                end do
-            end if
-            
-            ! Update data and size
+            new_data = 0.0d0
+            elements_to_copy = min(new_capacity, this%currentSize)
+            old_head = this%head
+            ! Copy the most recent elements in correct order
+            do i = 1, elements_to_copy
+                idx = mod(old_head - elements_to_copy - 1 + i, this%capacity) + 1
+                new_data(i) = this%data(idx)
+            end do
+            this%currentSize = elements_to_copy
+            this%head = mod(elements_to_copy, new_capacity) + 1
             if (allocated(this%data)) Deallocate(this%data)
             this%data = new_data
             this%capacity = new_capacity
@@ -117,18 +94,19 @@ Module ModuleTimeQueue
             end if
             this%currentSize = 0
             this%capacity = 0
+            this%head = 1
         End Subroutine Destroy
         
         ! Get all data in chronological order (oldest to newest)
         Function GetAllData(this) result(ordered_data)
             Class(TimeQueue), Intent(in)     :: this
             Real(8), Allocatable            :: ordered_data(:)
-            
-            Allocate(ordered_data(this%capacity))
-            ordered_data = 0.0d0
-            if (this%currentSize > 0) then
-                ordered_data(1:this%currentSize) = this%data(1:this%currentSize)
-            end if
+            Integer :: i, idx
+            Allocate(ordered_data(this%currentSize))
+            do i = 1, this%currentSize
+                idx = mod(this%head + i - 2 + this%capacity*10, this%capacity) + 1
+                ordered_data(i) = this%data(idx)
+            end do
         End Function GetAllData
 
         ! Get single element by chronological index (1 = oldest, size = newest)
@@ -136,15 +114,15 @@ Module ModuleTimeQueue
             Class(TimeQueue), Intent(in)     :: this
             Integer, Intent(in)             :: index
             Real(8)                        :: value
-            
+            Integer :: idx
             ! Check index range
             if (index < 1 .or. index > this%currentSize) then
                 print *, "Error: Index out of bounds in GetAt"
                 value = 0.0d0  ! Return default value
                 return
             end if
-            
-            ! Direct array access (index already in correct order)
-            value = this%data(index)
+            idx = mod(this%head + index - 2 + this%capacity*10, this%capacity) + 1
+            value = this%data(idx)
         End Function GetAt
 End Module ModuleTimeQueue
+
